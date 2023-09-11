370c5f31895b634e9f9d83416a4f0d7b39637df4
Trishan Gunness <tgunness@gmail.com>
2023-09-11 19:06:01 -0400

    added object

---
+++ Week2/Week2.InClassDemo/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2.InClassDemo
+{
+    public class Account
+    {
+        public Account()
+        {
+            
+        }
+        public Account(string name, string address,bool isActive)
+        {
+            Name = name;
+            Address = address;
+            this.isActive = isActive;
+        }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool isActive { get; set; }
+    }
+}
--- Week2/Week2.InClassDemo/Program.cs
+++ Week2/Week2.InClassDemo/Program.cs
@@ -13,32 +13,16 @@ namespace Week2.InClassDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Please enter your name:");
+            Account myAccount = new Account();
+            myAccount.Name = "Trishan";
+            myAccount.Address = "someAddress";
 
-            string fname = "Trishan";
-            string lname = "Gunness";
-
-            string fullname = "Mr. " + fname + " Middle Name " + lname;
-
-            string fullname2 = String.Format("Mr. {0} {1}", fname, lname);
-
-            string fullName = $"Mr. {fname} {lname}";
-            string filePath = @"C:\test\2";
-
-            string menu = 
-$@"Console application
-1. List Stuff
-2. Edit Stuff
-3. Move Stuff
-Please select stuff {fullName}:
-";
-
-
-            Console.WriteLine(menu);
-
-            Thread.Sleep(10000);
-
-
+            var myAccount3 = new Account()
+            {
+                Name = "Trishan",
+                Address = "someAddressIhave",
+                isActive = false
+            };
 
         }
     }
