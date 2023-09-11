8dd2fe3251b6715c0a774e8f04308881087c3843
Trishan Gunness <tgunness@gmail.com>
2023-09-11 18:36:34 -0400

    week 2 commit 1

--- Week2/Week2.InClassDemo/Program.cs
+++ Week2/Week2.InClassDemo/Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
+//this is inline comment
 
 namespace Week2.InClassDemo
 {
@@ -10,6 +13,36 @@ namespace Week2.InClassDemo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World! Please enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine(name);
+            var a = 12;
+            long b = a;
+            float c = a;
+            double d = a;
+            decimal currency = a;
+
+            int e = Convert.ToInt32(b);
+            int f = int.Parse(name);
+            int g;
+            bool canConvert = int.TryParse(name, out g);
+
+
+            if(int.TryParse(name, out int gg))
+            {
+                Console.WriteLine(gg);
+            }
+            Thread.Sleep(2000);
+
+
+            decimal myMoney = 300.5m;
+            float myRadius = 3.145f;
+
+            int aa = 12;
+            var bb = 12;
+            var cc = "";
+
         }
     }
 }
