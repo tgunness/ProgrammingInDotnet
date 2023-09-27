86b082e32e428c48468d906cbe0925e1a854176c
Trishan Gunness <tgunness@gmail.com>
2023-09-27 13:03:35 -0400

    added inclass demo

---
+++ InClassDemos/Week4/OldMacDonaldAppleFarm/OldMacDonaldAppleFarm.Common/Apple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OldMacDonaldAppleFarm.Common
+{
+    public class Apple
+    {
+        public Apple(int rad, int weight, string type)
+        {
+            this.Radius = rad;
+            this.Weight = weight;
+            this.Type = type;
+        }
+
+        public int Radius { get; set; }
+        public int Weight { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+
+        public int CalculateVolume()
+        {
+            int volume = (int)(4/3 * Math.PI * Radius * Radius * Radius);
+            return volume;
+        }
+    }
+}
