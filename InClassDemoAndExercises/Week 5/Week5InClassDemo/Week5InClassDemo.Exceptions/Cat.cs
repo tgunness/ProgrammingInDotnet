3444561d862e79beb0a94ea9836d3be928136e8c
Trishan Gunness <tgunness@gmail.com>
2023-10-04 14:01:10 -0400

    unfinished the exception demo

--- InClassDemoAndExercises/Week 5/Week5InClassDemo/Week5InClassDemo.Exceptions/Cat.cs
+++ InClassDemoAndExercises/Week 5/Week5InClassDemo/Week5InClassDemo.Exceptions/Cat.cs
@@ -8,30 +8,12 @@ namespace Week5InClassDemo.Exceptions
         public Cat() { }
         public Cat(int age)
         {
-            if (age == 0)
-                throw new InvalidAnimalException("age can not be zero", "Cat");
-
             _age = age;
         }
         public string CalculateAge()
         {
-            try
-            {
-                int convertedAge = 10 / _age;
-                return $"Cat is {convertedAge} years old";
-            }
-            catch(DivideByZeroException e)
-            {
-                return "unknown due to divide zero";
-            }
-            catch(ArgumentException e)
-            {
-                return "unknown due to invalid arg";
-            }
-            catch(Exception e)
-            {
-                return "unknown, for reasons unknown";
-            }
+            int convertedAge = 10 / _age;
+            return $"Cat is {convertedAge} years old";
         }
     }
 }
