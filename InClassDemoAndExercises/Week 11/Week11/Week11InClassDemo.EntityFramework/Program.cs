290953176c1b190008917d4e0e3726e7d79a9f19
Trishan Gunness <tgunness@gmail.com>
2023-11-22 18:12:06 -0500

    added inclass code 11.2

---
+++ InClassDemoAndExercises/Week 11/Week11/Week11InClassDemo.EntityFramework/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week11InClassDemo.EntityFramework
+{
+    public class NorthWindDbContext : DbContext
+    {
+        public DbSet<Order> Orders { get; set; }
+
+        public NorthWindDbContext(string connectionString) : base(connectionString)
+        {
+
+        }
+    }
+
+    // Entity class representing an Order
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerID { get; set; }
+        // Add other properties...
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new NorthWindDbContext("server=.\\SQLEXPRESS;Database=Northwind2;User Id=sa;Password=Password01!"))
+            {
+                foreach(var order in context.Orders)
+                {
+                    Console.WriteLine(order.OrderID);
+                }
+            }
+        }
+    }
+}
